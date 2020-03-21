e9f93e8f6ec6dacca0f82a3d5cbe4bd05f91b456
agehaofairy <agehaofairy@gmail.com>
2020-03-21 15:28:16 +0300

    start

---
+++ higher_school_of_hooyomics/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace higher_school_of_hooyomics.Models
+{
+    public class Product
+    {
+        public string Author { get; set; }
+
+        public string Country { get; set; }
+
+        public Uri ImageLink { get; set; }
+
+        public string Language { get; set; }
+
+        public int Pages { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public override string ToString() => "";
+    }
+}
