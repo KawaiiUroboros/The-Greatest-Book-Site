80af3651b79d020446acb1c08414193ee6d42d07
Artyom Snatenkov <artem_snatenkov@mail.ru>
2020-03-21 22:03:40 +0300

    new pages: Books

---
+++ higher_school_of_hooyomics/Views/Home/Books.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace higher_school_of_hooyomics.Views.Home
+{
+    public class Books : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
