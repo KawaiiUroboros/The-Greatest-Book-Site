622d93bab6e61d1363e163b68e8880f00a01f4e6
agehaofairy <agehaofairy@gmail.com>
2020-03-21 13:50:08 +0300

    started

---
+++ higher_school_of_hooyomics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace higher_school_of_hooyomics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
