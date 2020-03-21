1e41d93f845b7c844f94f01cace30c6c39a77dae
agehaofairy <agehaofairy@gmail.com>
2020-03-21 15:58:37 +0300

    model broken css

---
+++ higher_school_of_hooyomics/wwwroot/main.js
@@ -0,0 +1,66 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+
+
+	var
+		screen = 0,
+		container = $('.cont'),
+		pages = $('.page'),
+		inscroll = false;
+
+	$('.page:first-child').addClass('active');
+
+	$('body').on('mousewheel', function (event) {
+
+		console.log(event.deltaY);
+
+		var
+			activepage = pages.filter('.active');
+
+
+		if (!inscroll) {
+			inscroll = true;
+
+
+			if (event.deltaY > 0) {
+
+				if (activepage.prev().length) {
+					screen--;
+				}
+
+			}
+			else {
+				if (activepage.next().length) {
+					screen++;
+				}
+			}
+
+		}
+
+		var
+			position = (-screen * 100) + '%';
+
+
+
+		pages.eq(screen).addClass('active').siblings().removeClass('active');
+
+		container.css('top', position);
+
+
+
+		setTimeout(function () {
+			inscroll = false;
+		}, 300);
+
+
+
+
+	});
+
+
+
+
+});
