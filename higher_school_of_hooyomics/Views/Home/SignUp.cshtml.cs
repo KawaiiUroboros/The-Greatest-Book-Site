717da9945863aa9da68901b48a5161521a3a1432
Artyom Snatenkov <artem_snatenkov@mail.ru>
2020-03-22 13:10:16 +0300

    Add LogIn and SignUp2

---
+++ higher_school_of_hooyomics/Views/Home/SignUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace higher_school_of_hooyomics.Views.Home
+{
+    public class SignUp : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
